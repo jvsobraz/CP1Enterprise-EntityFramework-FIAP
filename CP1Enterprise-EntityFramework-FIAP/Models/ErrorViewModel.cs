0fee3172c6fa045638bca8da219b401965aba519
jvsobraz <jvsobraz@hotmail.com>
2023-08-23 10:41:34 -0300

    Add project files.

---
+++ CP1Enterprise-EntityFramework-FIAP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CP1Enterprise_EntityFramework_FIAP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
