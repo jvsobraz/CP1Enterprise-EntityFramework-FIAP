499f1c050cf7839ed716bfbfa31d4ec0faf5beb3
jvsobraz <jvsobraz@hotmail.com>
2023-09-05 20:22:33 -0300

    DbContext (Não Finalizado)

--- CP1Enterprise-EntityFramework-FIAP/Persistence/OracleDbContext.cs
+++ CP1Enterprise-EntityFramework-FIAP/Persistence/OracleDbContext.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CP1Enterprise_EntityFramework_FIAP.Web.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace CP1Enterprise_EntityFramework_FIAP.Persistence
+namespace CP1Enterprise_EntityFramework_FIAP.Web.Persistence;
+
+public class OracleDbContext : DbContext
 {
-    public class OracleDbContext
+    public OracleDbContext(DbContextOptions<OracleDbContext>
+        options) : base(options)
     {
-        public DbSet<Cartas> Cartas { get; set; }
+    }
 
-        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base (options) 
-        {
-        
-        }
+    public DbSet<Carta> Cartas { get; set; }
+    public DbSet<Colecao> Colecoes { get; set; }
+    public DbSet<Idioma> Idiomas { get; set; }
+    public DbSet<Ilustrador> Ilustradores { get; set; }
+    public DbSet<Links> Links { get; set; }
+    public DbSet<RegrasEspeciais> RegrasEspeciais { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Carta>()
+            .HasOne(r => r.Colecoes)
+            .WithMany(q => q.Idioma)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
