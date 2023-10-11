1c5eae24931c52b97fdad6e316433c090098bc3b
jvsobraz <jvsobraz@hotmail.com>
2023-10-11 15:16:57 -0300

    Delete models

--- CP1Enterprise-EntityFramework-FIAP/Models/Idioma.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace CP1Enterprise_EntityFramework_FIAP.Models
-{
-    public class Idioma
-    {
-        public string Id { get; set; }
-        public string Nome { get; set; }
-    }
-}
